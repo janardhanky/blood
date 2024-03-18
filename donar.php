f69e8d6dfa3281c3170bc0943bb9a0b7d8767a3f
nishanthkj <nishanthkj9@gmail.com>
2024-03-18 21:20:53 +0530

    Main

---
+++ donar.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Blood bank Management System</title>
+<link href="css/lightbox.css" rel="stylesheet" />
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--slider-->
+<link href="css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
+     <script src="js/jquery-1.11.0.min.js"></script>
+	<script src="js/lightbox.min.js"></script>
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="js/jquery.flexslider.js" type="text/javascript"></script>
+  
+ <script type="text/javascript">
+     $(function () {
+         SyntaxHighlighter.all();
+     });
+     $(window).load(function () {
+         $('.flexslider').flexslider({
+             animation: "slide",
+             animationLoop: false,
+             itemWidth: 210,
+             itemMargin: 5,
+             minItems: 2,
+             maxItems: 4,
+             start: function (slider) {
+                 $('body').removeClass('loading');
+             }
+         });
+     });
+  </script>
+</head>
+
+<body>
+ <div class="h_bg">
+<div class="wrap">
+<div class="header">
+		<div class="logo">
+			<h1><a href="index.php"></a></h1>
+		</div>
+	</div>
+</div>
+</div>
+<div class="nav_bg">
+<div class="wrap">
+		<?php require('header.php');?>
+	</div>
+
+        <div>
+
+          <div class="h_btm_bg">
+<div class="wrap">
+<div class="h_btm">
+	<div class="header-para">
+		<table cellpadding="0" cellspacing="0" width="700px">
+            <tr><td><img src="Images/donor1.png" height="80px" /></td></tr>
+             <tr><td>&nbsp;</td></tr>
+            
+            
+                <p style="font-size:20px">Donating blood is a simple thing to do, but it can make a big difference in the lives of others. The donation process from the time you arrive until the time you leave takes about 30 minutes. The donation itself is only about 8-10 minutes on average.
+The steps in the process are :</p><br />
+        <h1 style="font-size:40px">Before Donating Blood</h6>
+			<p style= "font-size:40px" class="top"><ul style="list-style-type:disc; color:#805752" >
+<li style= "font-size:20px">Eat something substantial before your appointment.</li>
+<li style= "font-size:20px">Have plenty of liquid the day before donation, especially in warm weather. In the 3 hours before donating, have at least 3 to 4 good-sized glasses of water/juice.</li>
+</ul></p><br />
+        <h6 style="font-size:40px">Registration of Donor</h6>
+			<p style="font-size:20px" class="top">Our volunteer/staff will ask you to fill a form with brief information on contact details and medical history.</p><br />	
+        	<h1 style="font-size:40px">Physical Health check-up</h1>
+        <p style="font-size:20px" class="top">You will be examined by a doctor to answer some questions about your health history.
+Your blood pressure and hemoglobin level will be checked.</p><br />	
+     <h6 style="font-size:40px">Blood Donation</h4>
+       <p style="font-size:20px" class="top">We will cleanse an area on your arm and insert a brand new sterile needle for the blood draw. This feels like a quick pinch and is over in seconds.</p><br />
+  <ul style="list-style-type:disc; color:#805752" >
+<li style="font-size:20px">The actual donation takes about 8-10 minutes, during which you will be seated comfortably.</li>
+<li style="font-size:20px">When a unit of blood (350 &#8211; 450 ml) has been collected, the donation is complete.</li>
+<li style="font-size:20px">A staff person will place a bandage on your arm</li>
+</ul><br />
+        <h6 style="font-size:40px">Safety</h4>
+<p style="font-size:20px" class="top">All equipment is sterile; needles are used only once and then discarded. In the great majority of individuals, a donation of 450 ml is less than 10% of your total body volume and may be given safely every 3 months. Your body keeps on discarding and replenishing blood all the time whether you give blood or not, so this amount is quickly replenished.</p><br />
+       <h6 style="font-size:40px">After Donation</h2><br />
+          <h4 style="font-size:40px">Relax & Refresh</h4>
+        <p style="font-size:20px"class="top">You can have something to eat and drink in and relax for about 15-20 minutes before departing. Within 24-48 hours your blood volume is completely restored.
+Apart from enjoying our refreshments and souvenir, other things to help you recover quickly include</p><br />
+       <ul style="list-style-type:disc; color:#805752" >
+<li style="font-size:20px">Drink plenty of water, fruit juice etc. and avoid alcohol for 8 hours after you&#8217;ve given blood.</li>
+<li style="font-size:20px">Avoid any strenuous activity or heavy lifting with the &#8216;donating arm&#8217; for 24 hours after you&#8217;ve given blood.</li>
+<li style="font-size:20px">If you become unwell in any way after giving blood, speak to the doctor in-charge straight away.</li>
+</ul> <br />
+        <h4 style="font-size:40px">Donate Blood Again</h4>
+        <p class="top"><ul style="list-style-type:disc; color:#805752" >
+<li style="font-size:20px">Any blood donors can give blood again after 3 months.</li>
+<li style="font-size:20px">Apheresis plasma or platelet donors can give blood in around 2-3 weeks time.</li>
+</ul></p><br />
+        <h4 style="font-size:40px">Spread the word</h4>
+        <p style="font-size:20px" class="top">Tell your friends, colleagues and family about the wonderful feeling you experience on donating blood and saving lives.
+<br />Enjoy the feeling of knowing that you helped save precious lives!</p>
+
+                </td>
+            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        
+      
+
+	  
+                     <td style="float:right; padding-bottom:50px; padding-left:90px">  <a href="index1.php"><br /><br /></br></br><br /><br /></a></td></tr></table></div></div></div>
+                      		<br /><br />				
+                            
+                        
+				
+                   <div class="clear"></div>	
+				</div>	
+
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+
+        </div>
+        <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+	<div class="f_nav">
+		<ul>
+			
+            <li><a href="aboutus.php">About</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+			
+            </ul>
+	</div>
+		<div class="copy">
+			<p class="title">© All Rights Reserved </p>
+		</div>
+         <div class="clear"></div>	
+				</div>	
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Blood bank Management System</title>
+<link href="css/lightbox.css" rel="stylesheet" />
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--slider-->
+<link href="css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
+     <script src="js/jquery-1.11.0.min.js"></script>
+	<script src="js/lightbox.min.js"></script>
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="js/jquery.flexslider.js" type="text/javascript"></script>
+  
+ <script type="text/javascript">
+     $(function () {
+         SyntaxHighlighter.all();
+     });
+     $(window).load(function () {
+         $('.flexslider').flexslider({
+             animation: "slide",
+             animationLoop: false,
+             itemWidth: 210,
+             itemMargin: 5,
+             minItems: 2,
+             maxItems: 4,
+             start: function (slider) {
+                 $('body').removeClass('loading');
+             }
+         });
+     });
+  </script>
+</head>
+
+<body>
+ <div class="h_bg">
+<div class="wrap">
+<div class="header">
+		<div class="logo">
+			<h1><a href="index.php"><img src="" alt=""></a></h1>
+		</div>
+	</div>
+</div>
+</div>
+<div class="nav_bg">
+<div class="wrap">
+		<?php require('header.php');?>
+	</div>
+
+        <div>
+
+          <div class="h_btm_bg">
+<div class="wrap">
+<div class="h_btm">
+	<div class="header-para">
+		      <img src="Images/cs3.PNG"  alt="cs3" style="display:block;margin-right:auto;margin-left:auto;"/>	
+		<p style="font-size:15px">Blood is a body fluid in the circulatory system of humans and other vertebrates that delivers necessary substances such as nutrients and oxygen to the cells, and transports metabolic waste products away from those same cells. 
+			Blood in the circulatory system is also known as peripheral blood, and the blood cells it carries, peripheral blood cells.
+			Nearly 7% of the body weight of a human is made up of blood.</p>
+	</div>  
+	
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+<div class="s_bg">
+<div class="wrap">
+<div class="main_cont">
+ <section class="slider">
+        <div class="flexslider carousel">
+          <ul class="slides">
+  	    		<li>
+  	    	    <img src="Images/slide3.PNG" />
+  	    		</li>
+  	    		<li>
+  	    	    <img src="Images/slide2.PNG" />
+  	    		</li>
+  	    		<li>
+  	    	    <img src="Images/slider2.jpg"/>
+  	    		</li>
+  	    		<li>
+  	    	    <img src="Images/slide4.PNG" />
+  	    		</li>
+  	    		<li>
+  	    	    <img src="Images/slider1.jpg" />
+  	    		</li>
+  	    		<li>
+  	    	    <img src="Images/slide5.PNG" />
+  	    		</li>
+  	    		<li>
+  	    	    <img src="Images/slide6.PNG" />
+  	    		</li>
+  	    		<li>
+  	    	    <img src="Images/slider7.jpg" />
+  	    		</li>
+          </ul>
+        </div>
+      </section>
+	<div class="ribben">
+	   <div class="l-triangle-top"></div>
+	   <div class="l-triangle-bottom"></div>
+		   <div class="rectangle"></div>
+	      <div class="r-triangle-top"></div>
+	   <div class="r-triangle-bottom"></div>
+	   <div class="clear"></div>
+	</div>
+<div class="main">
+	<div class="content">		
+			<h2>Blood bank:</h2>
+			<p style="font-size:15px">We welcome you to in our WebSite. If you are a donor , We appreciate you <a href="index1.php">signing up</a> online as a Donor. If you need blood we are happy to serve you. This blood donor list is hosted by <a href="index.php">www.lifesaver.com</a> on behalf of "Life Saver Blood Bank"
+as a public service without any profit motive.We celebrate World Blood Donor Day on 14 June every year. It was established across the countries in 2004, to spread the awareness for need to donate blood and thank the blood donors for their selfless work. These blood donation quotes will motivate you to donate blood and save somebody’s life. Be a hero, A real hero in someone’s life. There are various myths surrounding the blood donation activities. Actual awareness and right information can really benefit both the donor and recipient. Come forward and join the hands in this initiative.This is a free service. While the Organisers have taken all steps to obtain accurate and up-to-date information of potential blood donors, 
+the Organisers and ICM Computers do not guarantee accuracy of the information contained herein or the suitability of the persons listed as any liability for direct or consequential damage to any person using this blood donor list including 
+loss of life or damage due to infection of any nature arising out of blood transfusion from persons whose names have been listed in this website.
+We request donors to update contact details regularly.</p>
+	</div>
+	<div class="sidebar">
+			<h2>Latest News</h2>
+			<div class="blog_posts">
+
+    <marquee direction="up" scrolldelay="300"><table >
+<tr><td>
+                         <div class="blog_desc">
+						<div class="blog_heading">
+							<span style="font-weight:bold"><b>Govt allows blood donation camps amid short supply</b>
+<p style="font-size:15px">New Delhi: Faced with a shortage of blood and blood components across healthcare facilities in India, the Centre has allowed voluntary blood donation camps to be organised after following strict social distancing measures.</p></span>
+							
+						 </div>	
+                         <br />
+	<span style="font-weight:bold"><b>World Blood Donor Day: Poor health reduces women blood donors in India</b>
+<p style="font-size:15px">While the overall number of blood donors in India has grown over the years, women constitute only a tiny 10 percent share owing to health problems like pernicious anaemia and low haemoglobin levels or being underweight</p></span>
+					  </div>
+                        </td></tr>
+                   
+                </table></marquee>  
+				 
+					 <div class="clear"></div>	
+				</div>	
+			
+			
+	</div>
+	<div class="clear"></div>
+	</div>
+</div>
+</div>
+</div>
+
+        </div>
+        <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+	<div class="f_nav">
+		<ul>
+			
+            <li><a href="aboutus.php">About</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+			
+            </ul>
+	</div>
+		<div class="copy">
+			<p class="title">© All Rights Reserved </p>
+		</div>
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+</body>
+</html>
